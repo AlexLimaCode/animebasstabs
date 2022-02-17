d6b59c848beb2b93395615aef8198b2111f2e9fa
alejandrolima-cbqa <alejandro.lima@cbqasolutions.com>
2022-02-17 17:25:08 -0500

    #api

--- pages/API.php
+++ pages/API.php
@@ -11,59 +11,77 @@
     <div class="container">
     <?php
 
-        $API_Key = 'AIzaSyAisnsTgxl5Vn0b4A_CQdxRQTDRFxYIbBE';
+        // $API_Key = 'AIzaSyAisnsTgxl5Vn0b4A_CQdxRQTDRFxYIbBE';
 
-        $channelId = 'UCaFJvXfpLTA6bh5dMm95tvQ';
+        // $channelId = 'UCaFJvXfpLTA6bh5dMm95tvQ';
 
-        //$API_Url = 'https://www.googleapis.com/youtube/v3/search?key=' . $API_Key . '&channelId=' . $chanenl_id . '&part=snippet,id&order=date';
-        $API_Url = 'https://www.googleapis.com/youtube/v3/';
+        // //$API_Url = 'https://www.googleapis.com/youtube/v3/search?key=' . $API_Key . '&channelId=' . $chanenl_id . '&part=snippet,id&order=date';
+        // $API_Url = 'https://www.googleapis.com/youtube/v3/';
 
-        $parameter = [
-            'id'=> $channelId,
-            'part'=> 'contentDetails',
-            'key'=> $API_Key
-        ];
-        $channel_URL = $API_Url . 'channels?' . http_build_query($parameter);
-        $json_details = json_decode(file_get_contents($channel_URL), true);
-        //4echo print_r($json_details);
-        $playlist = $json_details['items'][0]['contentDetails']['relatedPlaylists']['uploads'];
-        echo print_r($playlist);
-        $parameter = [
-            'part'=> 'snippet',
-            'playlistId' => $playlist,
-            'maxResults'=> '50',
-            'key'=> $API_Key
-        ];
-        $channel_URL = $API_Url . 'playlistId?' . http_build_query($parameter);
-        $json_details = json_decode(file_get_contents($channel_URL), true);
+        // $parameter = [
+        //     'id'=> $channelId,
+        //     'part'=> 'contentDetails',
+        //     'key'=> $API_Key
+        // ];
+        // $channel_URL = $API_Url . 'channels?' . http_build_query($parameter);
+        // $json_details = json_decode(file_get_contents($channel_URL), true);
+        // //4echo print_r($json_details);
+        // $playlist = $json_details['items'][0]['contentDetails']['relatedPlaylists']['uploads'];
+        // echo print_r($playlist);
+        // $parameter = [
+        //     'part'=> 'snippet',
+        //     'playlistId' => $playlist,
+        //     'maxResults'=> '50',
+        //     'key'=> $API_Key
+        // ];
+        // $channel_URL = $API_Url . 'playlistId?' . http_build_query($parameter);
+        // $json_details = json_decode(file_get_contents($channel_URL), true);
         
-        $my_videos = [];
-        foreach($json_details['items'] as $video){
-            //$my_videos[] = $video['snippet']['resourceId']['videoId'];
-         $my_videos[] = array( 'v_id'=>$video['snippet']['resourceId']['videoId'], 'v_name'=>$video['snippet']['title'] );
-        }
+        // $my_videos = [];
+        // foreach($json_details['items'] as $video){
+        //     //$my_videos[] = $video['snippet']['resourceId']['videoId'];
+        //  $my_videos[] = array( 'v_id'=>$video['snippet']['resourceId']['videoId'], 'v_name'=>$video['snippet']['title'] );
+        // }
          
-        while(isset($json_details['nextPageToken'])){
-            $nxt_page_URL = $channel_URL . '&pageToken=' . $json_details['nextPageToken'];
-            $json_details = json_decode(file_get_contents($nxt_page_URL), true);
-            foreach($json_details['items'] as $video)
-                $my_videos[] = $video['snippet']['resourceId']['videoId'];
-        }
+        // while(isset($json_details['nextPageToken'])){
+        //     $nxt_page_URL = $channel_URL . '&pageToken=' . $json_details['nextPageToken'];
+        //     $json_details = json_decode(file_get_contents($nxt_page_URL), true);
+        //     foreach($json_details['items'] as $video)
+        //         $my_videos[] = $video['snippet']['resourceId']['videoId'];
+        // }
 
-        foreach($my_videos as $video){
+        // foreach($my_videos as $video){
  
-            if(isset($video)){
+        //     if(isset($video)){
                 
-         echo '<a href="https://www.youtube.com/watch?v='. $video['v_id'] .'" style="background: url(\'https://img.youtube.com/vi/'. $video['v_id'] .'/mqdefault.jpg\')">
-         <div>'. $video['v_name'] .'</div>
-         </a>';
+        //  echo '<a href="https://www.youtube.com/watch?v='. $video['v_id'] .'" style="background: url(\'https://img.youtube.com/vi/'. $video['v_id'] .'/mqdefault.jpg\')">
+        //  <div>'. $video['v_name'] .'</div>
+        //  </a>';
+        //     }
+        // }
+            
+        $API_key    = 'Ehz5aP6npV4JqiBAXaNOC7jUdQs';
+
+        $channelID  = 'UCaFJvXfpLTA6bh5dMm95tvQ';
+
+        $maxResults = 10;
+        echo "https://www.googleapis.com/youtube/v3/search?key=".$API_key."&channelId=" . $channelID . "&part=snippet,id&order=date";
+        $videoList = json_decode(file_get_contents("https://www.googleapis.com/youtube/v3/search?order=date&part=snippet&channelId='.$channelID.'&maxResults='.$maxResults.'&key='.$API_key."));
+        
+        foreach($videoList->items as $item){
+
+            if(isset($item->id->videoId)){
+     
+            echo '<div class="youtube-video">
+     
+                    <iframe width="280" height="150" src="https://www.youtube.com/embed/'.$item->id->videoId.'" frameborder="0" allowfullscreen></iframe>
+     
+                    <h2>'. $item->snippet->title .'</h2>
+     
+                </div>';
+     
             }
         }
-
-        ?>
-
-        
-    
     ?>
     </div>
 </body>
